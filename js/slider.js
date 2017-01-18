c15a285066fdc2e4a28ab768df295b4006239627
Alexey Rakitski <alexey62soft@gmail.com>
2017-01-18 04:12:39 +0300

    - "blog" section is added; - slider script rewrite/added to 'use script' conception; - some global/small fixes and cleanups;

--- js/slider.js
+++ js/slider.js
@@ -1,23 +1,43 @@
+'use strict';
+
 //By Aleksei Rakitski 2017
 
+//fields
+var firstSlideIndex = 1;
+var secondSlideIndex = 1;
 
 //for first slider
-function firstCurrentDiv(n) {
-    firstShowDivs(n);
-}
-
 function firstShowDivs(n) {
-    var i, firstSlideIndex = n;
-    var x = document.getElementsByClassName('mySlide firstInScript');
-    var dots = document.getElementsByClassName('dot firstInScript');
-    if (n > x.length) {firstSlideIndex = 1}    
-    if (n < 1) {firstSlideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
+    var i,
+        x = document.getElementsByClassName('mySlide firstInScript'),
+        dots = document.getElementsByClassName('dot firstInScript');
+    if (n > x.length) {firstSlideIndex = 1; }
+    if (n < 1) {firstSlideIndex = x.length; }
+    for (i = 0; i < x.length; i = i + 1) {
+        x[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (i = 0; i < dots.length; i = i + 1) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    x[firstSlideIndex-1].style.display = "block";  
-    dots[firstSlideIndex-1].className += " active";
+    x[firstSlideIndex - 1].style.display = "block";
+    dots[firstSlideIndex - 1].className += " active";
+}
+
+function firstCurrentDiv(n) {
+    firstShowDivs(firstSlideIndex = n);
+}
+
+//for second slider
+function secondShowDivs(n) {
+    var i, x = document.getElementsByClassName('mySlide secondInScript');
+    if (n > x.length) {secondSlideIndex = 1; }
+    if (n < 1) {secondSlideIndex = x.length; }
+    for (i = 0; i < x.length; i = i + 1) {
+        x[i].style.display = "none";
+    }
+    x[secondSlideIndex - 1].style.display = "block";
+}
+
+function secondPlusDiv(n) {
+    secondShowDivs(secondSlideIndex += n);
 }
