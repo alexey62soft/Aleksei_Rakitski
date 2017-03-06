35b33d5df21f9f9bb532a49868bfb3addb5e940a
Alexey Rakitski <alexey62soft@gmail.com>
2017-03-06 03:56:28 +0300

    Remove dist directory (not needed) Rewrite formatters and newsapi modules; Add initial readmore functionality (need write the component, formatter and other listeners)

--- RESTful API Task/src/js/modules/formatter.es6.js
+++ RESTful API Task/src/js/modules/formatter.es6.js
@@ -1,10 +1,11 @@
-const allEventsContainer = 'events-container'
-const anyEventContainer = 'any-event';
+const allEventsContainer = '.events-container'
+const anyEventContainer = '.any-event';
 
 export function createElementForAll(elem, index)
 {
-    document.getElementsByClassName(anyEventContainer)[0].style.display = 'none';
-    let htmlText = '<event-general><div class="row">'+
+    document.querySelector(anyEventContainer).style.display = 'none';
+    let newEvent = document.createElement('event-general');
+    newEvent.innerHTML = '<div class="row">'+
                     '<div class="col-first">' +
                         '<span class="month">' + new Date(elem.publishedAt).getMonth() +
                             '<span class="under">_</span></span>' + 
@@ -15,15 +16,15 @@ export function createElementForAll(elem, index)
                         '<h4>' + elem.title.substring(0, 19) + '...' +
                         '</h4><h5>' + elem.author + 
                         '</h5><h5>Descrition</h5><p class="descriptionitem">' + elem.description.substring(0, 117) + '...' +
-                        '</p><div id=' + index + ' class="button">View Event Details</div></div></div></event-general>'
-    let htmlCont = document.getElementsByClassName(allEventsContainer)[0];
-    htmlCont.insertAdjacentHTML('beforeEnd', htmlText);
+                        '</p><div id=' + index + ' class="button">View Event Details</div></div></div>';
+    let htmlCont = document.querySelector(allEventsContainer);
+    htmlCont.appendChild(newEvent);
     htmlCont.style.display = 'block';
 }
 
 export function createElementForAny(elem)
 {
-    document.getElementsByClassName('.events')[0].style.display = 'none';
+    document.querySelector('.events').style.display = 'none';
     let text = '<div><ul><li>Title: ' + elem.title + 
         '</li><li>Author: ' + elem.author + 
         '</li><li>Published at: ' + elem.publishedAt + 
@@ -31,8 +32,8 @@ export function createElementForAny(elem)
         '</li><li>Url: ' + elem.url + 
         '<li><img style="width: 200px" src=' + elem.urlToImage +
         '></li></ul></div>';
-    var htmlcont = document.getElementById('testAny');
+    var htmlcont = document.querySelector(anyEventContainer);
     htmlcont.innerHTML = "";
     htmlcont.insertAdjacentHTML('beforeEnd', text);
-    document.getElementById('testAny').style.display = 'block';
+    document.querySelector(anyEventContainer).style.display = 'block';
 }
--- RESTful API Task/src/js/modules/newsapi.es6.js
+++ RESTful API Task/src/js/modules/newsapi.es6.js
@@ -3,23 +3,27 @@ import * as formatter from './formatter.es6'
 const ApiKey = '22c7d2614bec413494f1ba7b26b43c89';
 const source = 'bbc-news';
 const sortBy = 'latest';
+let funcs = this;
 
 export function getAllNews(){
 	fetch('https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + ApiKey)
         .then((response) => response.json())
         .then((data) => {
             let index = 0;
-            data.articles.forEach(function(element) {
+            data.articles.forEach((element) => {
                 formatter.createElementForAll(element, index);
                 index++;
             }, this);
+            let elements = document.getElementsByTagName('event-general');
+            for (var i = 0; i < elements.length; i++) {
+                let button = elements[i].querySelector('.button');
+                button.addEventListener('click', () => {
+                    fetch('https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + ApiKey)
+                        .then((response) => response.json())
+                        .then((data) => button.id < data.articles.length ? formatter.createElementForAny(data.articles[button.id]): '')
+                        .catch((ex) => console.log('Parsing failed', ex));
+                }, false);
+            }
         })
-        .catch((ex) => console.log('Parsing failed', ex));
-}
-
-export function getAnyNews(num){
-	fetch('https://newsapi.org/v1/articles?source=' + source + '&sortBy=' + sortBy + '&apiKey=' + ApiKey)
-        .then((response) => response.json())
-        .then((data) => num < data.articles.length ? formatter.createElementForAny(data.articles[num]): '')
         .catch((ex) => console.log('Parsing failed', ex));
 }
