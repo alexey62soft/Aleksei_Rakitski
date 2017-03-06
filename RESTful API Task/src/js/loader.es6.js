db980b100609843b291321d59cfc87bb21a542a4
Alexey Rakitski <alexey62soft@gmail.com>
2017-03-06 12:53:37 +0300

    Continue)

--- RESTful API Task/src/js/loader.es6.js
+++ RESTful API Task/src/js/loader.es6.js
@@ -1,4 +1,15 @@
 import * as newsapi from './modules/newsapi.es6'
 
-newsapi.getAllNews();
+document.addEventListener("DOMContentLoaded", () => {
+    let prevEventsBtn = document.querySelector('.prev-events');
+    let nextEventsBtn = document.querySelector('.next-events');
+    prevEventsBtn.addEventListener('click', () => {
+        newsapi.changeSource(-1);
+    }, false);
+    nextEventsBtn.addEventListener('click', () => {
+        newsapi.changeSource(1);
+    }, false);
+
+    newsapi.changeSource(0);
+});
 
--- RESTful API Task/src/js/modules/newsapi.es6.js
+++ RESTful API Task/src/js/modules/newsapi.es6.js
@@ -1,15 +1,17 @@
 import * as formatter from './formatter.es6'
 
 const ApiKey = '22c7d2614bec413494f1ba7b26b43c89';
-const source = 'bbc-news';
-const sortBy = 'latest';
+const source = ['bbc-news', 'business-insider', 'the-sport-bible', 'google-news', 'national-geographic', 'new-scientist'];
+let indOfS = 0;
+const sort = 'top';
 let funcs = this;
 
 function gAllN() {
-    fetch('https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + ApiKey)
+    fetch('https://newsapi.org/v1/articles?source=' + source[indOfS] + '&sortBy=' + sort + '&apiKey=' + ApiKey)
         .then((response) => response.json())
         .then((data) => {
             let index = 0;
+            document.querySelector('.events-container').innerHTML = '';
             data.articles.forEach((element) => {
                 formatter.createElementForAll(element, index);
                 index++;
@@ -26,12 +28,30 @@ function gAllN() {
 }
 
 function gAnyN(id) {
-    fetch('https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + ApiKey)
+    fetch('https://newsapi.org/v1/articles?source=' + source[indOfS] + '&sortBy=' + sort + '&apiKey=' + ApiKey)
         .then((response) => response.json())
         .then((data) => {
             id < data.articles.length ? formatter.createElementForAny(data.articles[id]): '';
         })
         .catch((ex) => console.log('Parsing failed', ex));
+}
+
+function chSource(param) {
+    document.querySelector('.prev-events').style.display = 'none';
+    document.querySelector('.next-events').style.display = 'none';
+    if(indOfS + param == 0)
+    {
+        document.querySelector('.next-events').style.display = 'inline-block';
+    }
+    else if(indOfS + param == source.length - 1) {
+        document.querySelector('.prev-events').style.display = 'inline-block';
+    }
+    else if(indOfS + param > 0 && indOfS + param < source.length - 1) {
+        document.querySelector('.next-events').style.display = 'inline-block';
+        document.querySelector('.prev-events').style.display = 'inline-block';
+    }
+    indOfS = indOfS + param;
+    gAllN();
 }
 
 export function getAllNews(){
@@ -40,4 +60,8 @@ export function getAllNews(){
 
 export function getAnyNews(id){
     gAnyN(id);
+}
+
+export function changeSource(param){
+    chSource(param);
 }
